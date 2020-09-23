d2d54efc98fba000c8af2bfdf2f363dc9ae383c8
John El Basha <johnelbasha@gmail.com>
2020-09-23 16:50:05 +0100

    fixed routing problems. App is now working again. Needs code to be refactored and visual improvements

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # user stories to implement:
-  # 1- A visitor can see the list of all restaurants.
-  get "restaurants", to: "restaurants#index"
-  # 2- A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
-  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
-  post 'restaurants', to: 'restaurants#create'
-  # 3- A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
-  get "restaurants/:id", to: 'restaurants#show', as: :restaurant
-  # 4- A visitor can add a new review to a restaurant
-  get "restaurants/:restaurant_id/reviews/new", to: 'reviews#new', as: :new_restaurant_review
+  resources :restaurants, only: %i[index new create show] do
+    resources :reviews, only: %i[new create]
+  end
 end
+
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_restaurant_review
