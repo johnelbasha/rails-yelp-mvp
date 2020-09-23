983bc00ff57d158b1b619bebca792c18c1561c2c
John El Basha <johnelbasha@gmail.com>
2020-09-23 12:25:39 +0100

    finished user story #1 (A visitor can see the list of all restaurants.)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # user stories to implement:
+  # 1- A visitor can see the list of all restaurants.
+  get "restaurants", to: "restaurants#index"
+  # 2- A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+  # 3- A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+  # 4- A visitor can add a new review to a restaurant
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ bristol = Restaurant.new(name: "epicure", category: "french", address: "Paris")
+ bristol.save
+ strawberry = Restaurant.new(name: "strawberry", category: "japanese", address: "Tokyo")
+ strawberry.save
+ plum = Restaurant.new(name: "Mardi", category: "Italian", address: "Aberdeen")
+ plum.save
+ cherry = Restaurant.new(name: "Harry's", category: "belgian", address: "Monaco")
+ cherry.save
+ apple = Restaurant.new(name: "china", category: "chinese", address: "Manchester")
+ apple.save
+
+yummy = Review.new(content: "very good", rating: 2, restaurant_id: 1)
+yummy.save
+okay = Review.new(content: "very bad", rating: 1, restaurant_id: 2)
+okay.save
+good = Review.new(content: "good", rating: 3, restaurant_id: 3)
+good.save
+bad = Review.new(content: "bad", rating: 0, restaurant_id: 1)
+bad.save
+excellent = Review.new(content: "100%", rating: 5, restaurant_id: 1)
+excellent.save
+
+
