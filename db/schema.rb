574d1182151172ee3391d6af1f4af476347300cf
John El Basha <johnelbasha@gmail.com>
2020-09-23 11:22:31 +0100

    implementing restaurant model: only 1 error remaining to resolve

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2020_09_23_095442) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "content"
-    t.integer "star"
+    t.integer "rating"
     t.integer "restaurant_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
